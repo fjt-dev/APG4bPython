55bfab5d7bd84e34230b043d6a8fde72095db80a
fjt-dev <fjt.cloud@gmail.com>
2024-06-25 16:25:27 +0900

    Create EX9 - 電卓をつくろう2.py

---
+++ J - 1.09.for文・break・continue/EX9 - 電卓をつくろう2.py
@@ -0,0 +1,20 @@
+N = int(input())
+A = int(input())
+
+for i in range(N):
+  op, x = input().split()
+  x = int(x)
+  
+  if op == "+":
+    A += x
+  elif op == "-":
+    A -= x
+  elif op == "*":
+    A *= x
+  elif op == "/" and x != 0:
+    A //= x
+  else:
+    print("error")
+    break
+  
+  print(i + 1, A)
